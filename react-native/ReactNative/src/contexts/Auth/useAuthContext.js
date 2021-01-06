f582f0fc011a8dafd3d05b2f6d951602c0b9e69d
Cícero Viana <cicerohen@gmail.com>
2021-01-05 23:27:58 -0300

    remove ReactNative folder

--- react-native/ReactNative/src/contexts/Auth/useAuthContext.js
+++
@@ -1,7 +0,0 @@
-import {useContext} from 'react';
-
-import Context from './AuthContext';
-
-const useAuthContext = () => useContext(Context);
-
-export default useAuthContext;
--- react-native/ReactNative/src/styles.config.js
+++
@@ -1,7 +0,0 @@
-export const HORIZONTAL_PADDING = 20;
-export const VERTICAL_PADDING = 20;
-
-export const palette = {
-  primaryColor: '#264653',
-  secondaryColor: '#2a9d8f',
-};
